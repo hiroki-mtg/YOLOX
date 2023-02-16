160d155eda493cdc233b65426f2bf1f65f927902
hiroki-mtg <hirokimt5@gmail.com>
2023-02-16 15:24:38 -0800

    add yolox_s_domino.py

---
+++ exps/example/custom/yolox_s_domino.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+
+        # Model Config
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        self.num_classes = 71
+
+        # --------------------------------------------------
+
+        # Dataset Path (Default datasets/COCO)
+        self.data_dir = "../datasets/dominos"
+
+        # Annotation File (Under Annotations/)
+        self.train_ann = "train_ann.json"
+        self.val_ann = "val_ann.json"
+        self.test_ann = "test_ann.json"
+
+        # IMG Dir
+        self.train_name = "train"
+        self.val_name = "val"
+        self.test_name = "test"
+
+        # --------------------------------------------------
+
+        # Hyperparameters
+        self.max_epoch = 300
+        self.data_num_workers = 4
+        self.eval_interval = 10
+        self.save_history_ckpt = True
+
+
+    def get_dataset(self, cache: bool = False, cache_type: str = "ram"):
+        """
+        Get dataset according to cache and cache_type parameters.
+        Args:
+            cache (bool): Whether to cache imgs to ram or disk.
+            cache_type (str, optional): Defaults to "ram".
+                "ram" : Caching imgs to ram for fast training.
+                "disk": Caching imgs to disk for fast training.
+        """
+        from yolox.data import COCODataset, TrainTransform
+
+        return COCODataset(
+            data_dir=self.data_dir,
+            json_file=self.train_ann,
+            name=self.train_name,
+            img_size=self.input_size,
+            preproc=TrainTransform(
+                max_labels=50,
+                flip_prob=self.flip_prob,
+                hsv_prob=self.hsv_prob
+            ),
+            cache=cache,
+            cache_type=cache_type,
+        )
+    
+    def get_eval_dataset(self, **kwargs):
+        from yolox.data import COCODataset, ValTransform
+        testdev = kwargs.get("testdev", False)
+        legacy = kwargs.get("legacy", False)
+
+        return COCODataset(
+            data_dir=self.data_dir,
+            json_file=self.val_ann if not testdev else self.test_ann,
+            name=self.val_name if not testdev else self.test_name,
+            img_size=self.test_size,
+            preproc=ValTransform(legacy=legacy),
+        )
